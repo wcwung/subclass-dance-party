85779f0a9d6c85495b021826672bf4b13baece41
wcwung <williamcwung@gmail.com>
2015-02-09 17:21:25 -0800

    Refactor functional into psuedo-classical

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -6,7 +6,8 @@ describe("blinkyDancer", function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    // debugger;
+    blinkyDancer = new BlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it("should have a jQuery $node object", function(){
@@ -22,7 +23,9 @@ describe("blinkyDancer", function() {
   describe("dance", function(){
     it("should call step at least once per second", function(){
       sinon.spy(blinkyDancer, "step");
+      // debugger;
       expect(blinkyDancer.step.callCount).to.be.equal(0);
+      // console.log(blinkyDancer);
       clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
       clock.tick(timeBetweenSteps);
 
